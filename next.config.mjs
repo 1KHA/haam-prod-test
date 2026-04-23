f95ddbebfee596510317f07a521e82bbb4d6868d
Khalid Abdulghani <k.h.abdulghani@gmail.com>
2026-04-23 16:21:52 +0300

    fix build error 04

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   serverExternalPackages: ['handlebars'],
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /node_modules\/handlebars/ },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
